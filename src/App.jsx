37350f6a8da1e94fd0c807929d3cf201c85d6b63
ankitad7123 <ankit11akhil@gmail.com>
2024-09-27 16:30:44 +0530

    baseurl

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
     <Router>
       <Center>
         <Box bg="white" height="100vh">
-          <Routes>
+          <Routes basename="/">
             {/* Redirect to Selection if user is logged in */}
             <Route
               path="/"
